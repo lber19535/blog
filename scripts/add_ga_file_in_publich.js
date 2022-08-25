a6fb77276169402bf59230df3bb95e554bf5dc7f
Bill <blv@xiaohongshu.com>
2022-08-25 22:42:45 +0800

    fix:git actions error

--- scripts/add_ga_file_in_publich.js
+++ scripts/add_ga_file_in_publich.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 hexo.on('deployBefore', () => {
     const gaFilePath = hexo.config['google_analytics']['ga_file_path']
-    const gaFileName = gaFilePath.split("/").at(-1)
+    const gaFileName = gaFilePath.split('/').at(-1)
 
     const sourceGAFilePath = hexo.base_dir + gaFilePath
     const dstGAFilePath = hexo.public_dir + gaFileName
