903b46c916e39779e15a863668594ffa86ebf474
Bill <blv@xiaohongshu.com>
2022-08-26 12:08:06 +0800

    fix:deploy error

--- scripts/add_ga_file_in_publich.js
+++ scripts/add_ga_file_in_publich.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
 
-Array.prototype.at = function(n) {
-    // ToInteger() abstract op
-	n = Math.trunc(n) || 0;
-	// Allow negative indexing from the end
-	if (n < 0) n += this.length;
-	// OOB access is guaranteed to return undefined
-	if (n < 0 || n >= this.length) return undefined;
-	// Otherwise, this is just normal property access
-	return this[n];
-}
+// Array.prototype.at = function(n) {
+//     // ToInteger() abstract op
+// 	n = Math.trunc(n) || 0;
+// 	// Allow negative indexing from the end
+// 	if (n < 0) n += this.length;
+// 	// OOB access is guaranteed to return undefined
+// 	if (n < 0 || n >= this.length) return undefined;
+// 	// Otherwise, this is just normal property access
+// 	return this[n];
+// }
 
 hexo.on('deployBefore', () => {
     const gaFilePath = hexo.config['google_analytics']['ga_file_path']
