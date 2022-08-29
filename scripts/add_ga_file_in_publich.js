583ef875eae7868aa2cb8d0602210ba16cc40dbf
Bill <blv@xiaohongshu.com>
2022-08-29 11:03:50 +0800

    fix:depoly error

--- scripts/add_ga_file_in_publich.js
+++ scripts/add_ga_file_in_publich.js
@@ -1,19 +1,9 @@
 const fs = require('fs');
 
-// Array.prototype.at = function(n) {
-//     // ToInteger() abstract op
-// 	n = Math.trunc(n) || 0;
-// 	// Allow negative indexing from the end
-// 	if (n < 0) n += this.length;
-// 	// OOB access is guaranteed to return undefined
-// 	if (n < 0 || n >= this.length) return undefined;
-// 	// Otherwise, this is just normal property access
-// 	return this[n];
-// }
-
 hexo.on('deployBefore', () => {
     const gaFilePath = hexo.config['google_analytics']['ga_file_path']
-    const gaFileName = gaFilePath.split('/').at(-1)
+    let pathArray = gaFilePath.split('/')
+    const gaFileName = pathArray[pathArray.length - 1]
 
     const sourceGAFilePath = hexo.base_dir + gaFilePath
     const dstGAFilePath = hexo.public_dir + gaFileName
